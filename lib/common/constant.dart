0c2009a3a3c30eb7302afe0c33b243d02c9f9fcc
Tuân Đỗ <dongoctuan2002sp@gmail.com>
2024-03-09 10:24:50 +0700

    Update constant.dart

--- lib/common/constant.dart
+++ lib/common/constant.dart
@@ -2,8 +2,8 @@ import 'package:progress_state_button/progress_button.dart';
 
 import 'enums.dart';
 
-const String apiDomain = "https://url.stormx.tech/";
-const String baseApiUrl = "https://url.stormx.tech/api/v1/";
+const String apiDomain = "https://url.stormx.space/";
+const String baseApiUrl = "https://url.stormx.space/api/v1/";
 const String apiAuthUrl = "${baseApiUrl}auth";
 const String apiUserUrl = "${baseApiUrl}user";
 const String apiUrl = "${baseApiUrl}url";
