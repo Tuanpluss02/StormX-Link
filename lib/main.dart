64693e23fb4d913ca5ad062f2c8f747e15a4a120
Tuanpluss02 <dongoctuan2002sp@gmail.com>
2024-03-25 05:52:24 +0700

    (feat) add import for flutter/foundation package and conditionally enable SystemUiMode

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -17,8 +18,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
-        overlays: [SystemUiOverlay.bottom]);
+    if (!kIsWeb) {
+      SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+          overlays: [SystemUiOverlay.bottom]);
+    }
     return MultiBlocProvider(
       providers: [
         BlocProvider<AuthBloc>(
